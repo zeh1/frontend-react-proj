afcd902f7bb75dcfb42b5b324fc23367500b3e44
crispy chili <zeh1xz@gmail.com>
2020-10-27 16:47:01 -0700

    start

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
 import React, { useEffect } from 'react';
 import './App.css';
 
-import Test from './components/test'
+
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      date: 'original state',
-      input: 'original input'
-    }
-    console.log('constructed')
   }
-  componentDidMount() {
-    this.setState({
-      date: new Date().toLocaleString()
-    })
-    console.log('mounted')
-  }
-  componentDidUpdate() {
-    console.log('updated')
-  }
-  handleinput = (e) => {
-    console.log('input event')
-    
-    this.setState((prev, props)=>{
-      console.log(`prev state: ${prev.input}`)
-      console.log(`props: ${Object.keys(props).length === 0 && props.constructor === Object}`)
-      return {
-        input: e.target.value
-      }
-    })
-    console.log(`after input ${this.state.date}`)
-  }
+
   render() {
     return (
-      <React.Fragment>
-        <p className="App-p">{this.state.date}</p>
-        <Test put={this.state.input} test={5}></Test>
-        <input type="text" onInput={this.handleinput}></input>
-        <p>input is {this.state.input}</p>
-      </React.Fragment>
+      <p>clean</p>
     )
   }
 }
