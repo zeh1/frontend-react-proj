732047bdadb71cce55860e2554a50919d3a2ec2e
crispy chili <zeh1xz@gmail.com>
2020-10-27 15:53:13 -0700

    will NOT rerender refetch if fetch on didmount, prev state will be kept

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import React, { useEffect } from 'react';
 import './App.css';
 
-import './components/test'
+import Test from './components/test'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      date: 'original state'
+      date: 'original state',
+      input: 'original input'
     }
     console.log('constructed')
   }
   componentDidMount() {
     this.setState({
-      date: 'original state'
+      date: new Date().toLocaleString()
     })
     console.log('mounted')
   }
   componentDidUpdate() {
     console.log('updated')
   }
+  handleinput = (e) => {
+    console.log('input event')
+    this.setState({
+      input: e.target.value
+    })
+    console.log(`${this.state.date}`)
+  }
   render() {
     return (
       <React.Fragment>
         <p className="App-p">{this.state.date}</p>
         <Test></Test>
+        <input type="text" onInput={this.handleinput}></input>
+        <p>input is {this.state.input}</p>
       </React.Fragment>
     )
   }
